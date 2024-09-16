b8226f6ffdf556e9002392a34a16bc77f7a0c4d7
Émilia B <89926033+BahAilime@users.noreply.github.com>
2024-09-16 16:08:02 +0200

    Fix link to repo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,7 +46,7 @@ const config: Config = {
 					// routeBasePath: "/",
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/bahEmilia/EPSIcours/blob/main",
+					editUrl: "https://github.com/bahEmilia/coursEPSI/blob/main",
 				},
 				// blog: false,
 				blog: {
